8b279a6238e6daa9b2e04ce08706385429daa4b7
Konstantinos Kasidiaris <kkasidiaris@gmail.com>
2020-03-25 23:52:12 +0200

    Updates

---
+++ precache-manifest.b3a0ccfc4496bb8ea9e902e4bdbce015.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477a46118b8237f53aa746a9469b32c4",
+    "url": "/gangstabots/index.html"
+  },
+  {
+    "revision": "e1aca60f3c4104ce23c2",
+    "url": "/gangstabots/static/css/main.ed358cab.chunk.css"
+  },
+  {
+    "revision": "4ef5719bddd27a20e339",
+    "url": "/gangstabots/static/js/2.3a31961d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gangstabots/static/js/2.3a31961d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1aca60f3c4104ce23c2",
+    "url": "/gangstabots/static/js/main.35b80ba7.chunk.js"
+  },
+  {
+    "revision": "6107553384257795471c",
+    "url": "/gangstabots/static/js/runtime-main.cc726ce2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/gangstabots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
